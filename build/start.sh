1bfbe45f3fb3b40ac1e8e28a27b4d73191973217
まぐにあ <dev@mail.mag-neko.net>
2022-10-01 10:48:20 +0900

    コンフィグを起動時に作成するよう変更

--- build/start.sh
+++ build/start.sh
@@ -2,16 +2,15 @@
 
 if [ ! -e /opt/first ]
 then
-    cd EPGStation
-    cp config/config.yml.template config/config.yml
-    cp config/operatorLogConfig.sample.yml config/operatorLogConfig.yml
-    cp config/epgUpdaterLogConfig.sample.yml config/epgUpdaterLogConfig.yml
-    cp config/serviceLogConfig.sample.yml config/serviceLogConfig.yml
-    cp config/enc.js.template config/enc.js
+    cd /opt/EPGStation
+    cp config_template/config.yml.template config/config.yml
+    cp config_template/operatorLogConfig.sample.yml config/operatorLogConfig.yml
+    cp config_template/epgUpdaterLogConfig.sample.yml config/epgUpdaterLogConfig.yml
+    cp config_template/serviceLogConfig.sample.yml config/serviceLogConfig.yml
+    cp config_template/enc.js.template config/enc.js
     sed -i -e "s/mirakurunPath: http+unix:\/\/%2Fvar%2Frun%2Fmirakurun\.sock\//mirakurunPath: http:\/\/${MIRAKURUN_ENDPOINT}/" config/config.yml
     touch /opt/first
 fi
-
 
 cd /opt/EPGStation
 npm start
