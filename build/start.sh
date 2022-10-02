2512e778f660cdf95beb455deb932f126eb954c7
まぐにあ <40308331+ponta0@users.noreply.github.com>
2022-10-02 20:36:38 +0900

    フラグのパスを修正

--- build/start.sh
+++ build/start.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-if [ ! -e /opt/first ]
+if [ ! -e /opt/config/first ]
 then
     cd /opt/EPGStation
     cp config_template/config.yml.template config/config.yml
@@ -9,7 +9,7 @@ then
     cp config_template/serviceLogConfig.sample.yml config/serviceLogConfig.yml
     cp config_template/enc.js.template config/enc.js
     sed -i -e "s/mirakurunPath: http+unix:\/\/%2Fvar%2Frun%2Fmirakurun\.sock\//mirakurunPath: http:\/\/${MIRAKURUN_ENDPOINT}/" config/config.yml
-    touch /opt/first
+    touch /opt/config/first
 fi
 
 cd /opt/EPGStation
