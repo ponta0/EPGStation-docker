2abfd30306a9388e206e5ef4306c3d24ab3d2a31
まぐにあ <dev@mail.mag-neko.net>
2023-02-10 13:28:17 +0900

    設定ファイルの修正

--- build/start.sh
+++ build/start.sh
@@ -1,15 +1,16 @@
 #!/bin/env bash
 
-if [ ! -e /opt/config/first ]
+if [ ! -e /opt/EPGStation/config/first ]
 then
     cd /opt/EPGStation
+    cp config_template/* config/
     cp config_template/config.yml.template config/config.yml
     cp config_template/operatorLogConfig.sample.yml config/operatorLogConfig.yml
     cp config_template/epgUpdaterLogConfig.sample.yml config/epgUpdaterLogConfig.yml
     cp config_template/serviceLogConfig.sample.yml config/serviceLogConfig.yml
     cp config_template/enc.js.template config/enc.js
     sed -i -e "s/mirakurunPath: http+unix:\/\/%2Fvar%2Frun%2Fmirakurun\.sock\//mirakurunPath: http:\/\/${MIRAKURUN_ENDPOINT}/" config/config.yml
-    touch /opt/config/first
+    touch /opt/EPGStation/config/first
 fi
 
 cd /opt/EPGStation
